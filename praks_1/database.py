4c13507ca5095882717a14d47c30d396eeb7318e
Hans-Toomas Saarest <hanstoomas.saarest@gmail.com>
2017-02-21 17:23:30 +0200

    JSON comma and update method added

--- praks_1/database.py
+++ praks_1/database.py
@@ -19,9 +19,20 @@ def make_query(query):
     return create_objects(cursor.fetchall())
 
 
+def update(program):
+    cursor = db_connect()
+    sql = "UPDATE programmeerimiskeel SET name = %s, year = %s, designer = %s WHERE id = %s"
+    cursor.execute(sql, (program.name, program.year, program.designer, program.id))
+    cursor.commit()
+    cursor.close()
+
+
 def create_objects(records):
     result = ""
-    for record in records:
+    size = len(records)
+    for index, record in enumerate(records):
         prlng = programminglanguage.ProgrammingLanguage(record[0], record[1], int(record[2]), record[3])
         result += (json.dumps(prlng.serialize()))
+        if index < size -1:
+            result += ','
     return result
